be71e4083e04dc5c5d2a80d41104a5cf140394fe
Elio8T <elio_thadhani8@outlook.com>
2022-11-01 15:10:47 -0400

    made the player bord and variable for player turn

--- src/index.js
+++ src/index.js
@@ -4,5 +4,65 @@ import './index.css';
 import ShoppingList from "./ShoppingList";
 
 
+class Game extends React.Component{
+    render(){
+        return(
+            <div className = "game">
+                <div>
+                    
+                    <Board />
+                </div>
+            </div>
+        );
+    }
+
+}
+class Board extends React.Component{
+    
+    renderSquare(i){
+        return <Square />
+    }
+    render(){
+        const status = 'x'
+        return(
+            <div className = "board">
+                <div>Next Player: {status}</div>
+                <div className = "first-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+
+                </div>
+                <div className = "second-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+
+                </div>
+                <div className = "third-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+
+                </div>
+
+            </div>
+
+
+        );
+    }
+}
+class Square extends React.Component{
+    render(){
+        return(
+            <button className="square">
+                {/*todo*/}
+            </button>
+        );
+    }
+}
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ShoppingList name="Elio" item="water"/>);
+root.render(<Game />);
